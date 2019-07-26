689c1edad5231b671470a437f1d8e24df5204d2f
ziv <north780316@gamil.com>
2019-07-26 09:12:17 +0800

    [Description] update
    [Commit Files]
    x86_64/sse/simd.c
    x86_64/sse/sisd.c
    
    -------------------------------------------

--- x86_64/sse/simd.c
+++ x86_64/sse/simd.c
@@ -7,9 +7,8 @@ int main ( int argc, char **argv )
 	double x[] = {1.0, 2.0};
 	double y[] = {1e-10, 2e-10};
 	double z[] = {1e-11, 2e-11};
-	register long int iter = 500000000;
+	long int iter = 500000000;
 
-	// FPU 80 bit LDBL_EPSILON 2^-63 = 1.08420217248550443401e-19L;
 	__asm__( "movlps %0, %%xmm0" : "+m"(x[0]) ); // xmm0.v2_double[0] = x[0]
 	__asm__( "movhps %0, %%xmm0" : "+m"(x[1]) ); // xmm0.v2_double[1] = x[1]
 	__asm__( "movlps %0, %%xmm1" : "+m"(y[0]) ); // xmm1.v2_double[0] = y[0]
@@ -17,18 +16,30 @@ int main ( int argc, char **argv )
 	__asm__( "movlps %0, %%xmm2" : "+m"(z[0]) ); // xmm2.v2_double[0] = z[0]
 	__asm__( "movhps %0, %%xmm2" : "+m"(z[1]) ); // xmm2.v2_double[1] = z[1]
 
-	// use SIMD to accumulate
-	for ( register long int i = 0; i < iter; ++i )
-	{
-		__asm__( "addpd %xmm1, %xmm0" ); 
-		__asm__( "subpd %xmm2, %xmm0" );
-	}
+	__asm__( "xorq %rcx, %rcx" ); 
+	__asm__( "movq %0, %%r8" : "+m"(iter) ); 
+	__asm__( "jmp loop_cmp" );
+
+// --------------------------------------------------------------------------
+
+	// use SIMD to accumulate x[i] += y[i] - z[i]
+	__asm__( "loop_start:" ); 
+	__asm__( "addpd %xmm1, %xmm0" ); 
+	__asm__( "subpd %xmm2, %xmm0" );
+
+// --------------------------------------------------------------------------
+
+	__asm__( "incq %rcx" ); // ++i
+	__asm__( "loop_cmp:" );
+	__asm__( "cmpq %r8, %rcx" ); // i <= iter
+	__asm__( "jle loop_start" );
 
 	__asm__( "movlps %%xmm0, %0" : "+m"(x[0]) ); // x[0] = xmm0.v2_double[0] 
 	__asm__( "movhps %%xmm0, %0" : "+m"(x[1]) ); // x[1] = xmm0.v2_double[1] 
 
 	printf( "SIMD accumulate results:\n");
 	printf( "x[0] = %.16le\n", x[0] );
+
 	printf( "x[1] = %.16le\n", x[1] );
 
 	return EXIT_SUCCESS;
--- x86_64/sse/sisd.c
+++ x86_64/sse/sisd.c
@@ -7,14 +7,37 @@ int main ( int argc, char **argv )
 	double x[] = {1.0, 2.0};
 	double y[] = {1e-10, 2e-10};
 	double z[] = {1e-11, 2e-11};
-	register long int iter = 500000000;
+	long int iter = 500000000;
 
-	// use SIMD to accumulate
-	for ( register long int i = 0; i < iter; ++i )
-	{
-		x[0] += y[0] - z[0];
-		x[1] += y[1] - z[1];
-	}
+	__asm__( "movsd %0, %%xmm0" : "+m"(x[0]) ); 
+	__asm__( "movsd %0, %%xmm1" : "+m"(x[1]) ); 
+	__asm__( "movsd %0, %%xmm2" : "+m"(y[0]) ); 
+	__asm__( "movsd %0, %%xmm3" : "+m"(y[1]) ); 
+	__asm__( "movsd %0, %%xmm4" : "+m"(z[0]) ); 
+	__asm__( "movsd %0, %%xmm5" : "+m"(z[1]) ); 
+
+	__asm__( "xorq %rcx, %rcx" ); 
+	__asm__( "movq %0, %%r8" : "+m"(iter) ); 
+	__asm__( "jmp loop_cmp" );
+
+// --------------------------------------------------------------------------
+
+	// use SISD to accumulate x[i] += y[i] - z[i]
+	__asm__( "loop_start:" ); 
+	__asm__( "addsd %xmm2, %xmm0" ); 
+	__asm__( "addsd %xmm3, %xmm1" ); 
+	__asm__( "subsd %xmm4, %xmm0" ); 
+	__asm__( "subsd %xmm5, %xmm1" ); 
+
+// --------------------------------------------------------------------------
+
+	__asm__( "incq %rcx" ); // ++i
+	__asm__( "loop_cmp:" );
+	__asm__( "cmpq %r8, %rcx" ); // i <= iter
+	__asm__( "jle loop_start" );
+
+	__asm__( "movsd %%xmm0, %0" : "+m"(x[0]) ); 
+	__asm__( "movsd %%xmm1, %0" : "+m"(x[1]) ); 
 
 	printf( "SISD accumulate results:\n");
 	printf( "x[0] = %.16le\n", x[0] );
