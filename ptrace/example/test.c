2a9d102e073ef2d50c8a455bbfcaf7a304defc98
ziv <north780316@gamil.com>
2019-11-21 09:17:17 +0800

    [Description] add single instructino debug
    [Commit Files]
    ptrace/example/Makefile
    ptrace/example/demo1.c
    ptrace/example/demo2.c
    ptrace/example/test.c
    ptrace/example/write.c
    
    -------------------------------------------

---
+++ ptrace/example/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int main ( int argc, char **argv )
+{
+	int a = 1;
+	int b = 2;
+	int c = a + b;
+	printf( "c=%d\n", c );
+
+	return EXIT_SUCCESS;
+}
+
